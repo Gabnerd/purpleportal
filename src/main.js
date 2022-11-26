566c96d75bf651e57037b18ebd74782a488a6767
Gabriel Pescaroli <shaickmiles@gmail.com>
2022-11-26 14:16:49 -0300

    bootstrap

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 import "bootstrap/dist/css/bootstrap.min.css"
-import './assets/main.css'
 import "bootstrap"
+import './assets/main.css'
 
 const app = createApp(App)
 
