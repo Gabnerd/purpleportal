31d3abb6a1faa03e58d2e99b8566f35ea7e9b4d8
Gabriel Pescaroli <shaickmiles@gmail.com>
2022-11-27 01:21:55 -0300

    loading

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <script setup>
 import PostComponent from '../components/PostComponent.vue'
 import axios from 'axios';
-import {ref, reactive, onMounted} from 'vue';
+import { ref, reactive } from 'vue';
 
 const posts = reactive([]);
 
 
-axios.get("https://dummyjson.com/posts").then((res)=>{
+axios.get("https://dummyjson.com/posts").then((res) => {
   (res.data.posts).forEach(post => {
-    axios.get(`https://dummyjson.com/users/${post.userId}?select=username,image`).then((res2)=>{
-      posts.push({id: post.id ,title: post.title, body: post.body, tags: post.tags, username: res2.data.username, imgurl: res2.data.image});
+    axios.get(`https://dummyjson.com/users/${post.userId}?select=username,image`).then((res2) => {
+      posts.push({ id: post.id, title: post.title, body: post.body, tags: post.tags, username: res2.data.username, imgurl: res2.data.image });
     });
-    });
+  });
 });
 
 </script>
@@ -19,11 +19,12 @@ axios.get("https://dummyjson.com/posts").then((res)=>{
 <template>
   <main class="row align-items-center justify-content-center mx-0">
     <div class="mt-2" style="display: flex; justify-content: flex-end; width: 80%;">
-      <button>+</button>
+      <button class="btn rounded-circle btn-success border text-white">+</button>
     </div>
     <div class="flex-col align-items-center justify-content-center mt-2" style="width: 80%;">
-    <PostComponent v-for="post in posts" :id="post.id" :titulo="post.title" :body="post.body" :tags="post.tags" :autor="post.username" class="border px-2 rounded mb-2" :imgurl="post.imgurl" style="width: 100%;"/>
-   
-  </div>
+      <PostComponent v-for="post in posts" :id="post.id" :titulo="post.title" :body="post.body" :tags="post.tags"
+        :autor="post.username" class="border px-2 rounded mb-2" :imgurl="post.imgurl" style="width: 100%;" />
+
+    </div>
   </main>
 </template>
